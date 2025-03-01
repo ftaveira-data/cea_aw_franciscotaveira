1d4e21b75dcdb30660c736603f92512bda7866b8
Francisco Taveira <ftaveira.dev@gmail.com>
2025-03-01 00:41:37 +0000

    contrucao dos modelos sales stg finalizada

---
+++ models/staging/sales/stg_sales__salesorders.sql
@@ -0,0 +1,36 @@
+WITH sales_order_raw AS (
+    SELECT *
+    FROM {{ source('sales', 'SalesOrderHeader') }}
+)
+
+, renamed AS (
+    SELECT
+        CAST(SalesOrderID AS INT) AS pk_sales_order  -- Chave primária do pedido
+      , CAST(RevisionNumber AS INT) AS revision_number  -- Número da revisão do pedido
+      , CAST(OrderDate AS TIMESTAMP) AS order_date  -- Data do pedido
+      , CAST(DueDate AS TIMESTAMP) AS due_date  -- Data de vencimento do pedido
+      , CAST(ShipDate AS TIMESTAMP) AS ship_date  -- Data de envio do pedido
+      , CAST(Status AS INT) AS order_status  -- Status do pedido
+      , CAST(OnlineOrderFlag AS BOOLEAN) AS is_online_order  -- Indica se o pedido foi feito online
+      , CAST(PurchaseOrderNumber AS STRING) AS purchase_order_number  -- Número do pedido de compra
+      , CAST(AccountNumber AS STRING) AS account_number  -- Número da conta associada ao pedido
+      , CAST(CustomerID AS INT) AS fk_customer  -- FK para a tabela Customer
+      , CAST(SalesPersonID AS INT) AS fk_salesperson  -- FK para a tabela SalesPerson
+      , CAST(TerritoryID AS INT) AS fk_territory  -- FK para a tabela SalesTerritory
+      , CAST(BillToAddressID AS INT) AS fk_bill_address  -- FK para endereço de cobrança
+      , CAST(ShipToAddressID AS INT) AS fk_ship_address  -- FK para endereço de entrega
+      , CAST(ShipMethodID AS INT) AS fk_ship_method  -- FK para a tabela ShipMethod
+      , CAST(CreditCardID AS INT) AS fk_credit_card  -- FK para a tabela CreditCard
+      , CAST(CreditCardApprovalCode AS STRING) AS credit_card_approval_code  -- Código de aprovação do cartão de crédito
+      , CAST(CurrencyRateID AS INT) AS fk_currency_rate  -- FK para a tabela CurrencyRate
+      , CAST(SubTotal AS FLOAT) AS subtotal  -- Valor do subtotal (sem taxas)
+      , CAST(TaxAmt AS FLOAT) AS tax_amount  -- Valor do imposto aplicado
+      , CAST(Freight AS FLOAT) AS freight_cost  -- Custo do frete
+      , CAST(TotalDue AS FLOAT) AS total_due  -- Valor total do pedido
+      , CAST(Comment AS STRING) AS order_comment  -- Comentários adicionais sobre o pedido
+      , UPPER(CAST(RowGuid AS STRING)) AS id_unique  -- Identificador único do pedido
+      , DATE_TRUNC('DAY', CAST(ModifiedDate AS TIMESTAMP)) AS modified_date  -- Data de última modificação
+    FROM sales_order_raw
+)
+
+SELECT * FROM renamed
--- models/staging/sales/stg_sales__salesperson.sql
+++ models/staging/sales/stg_sales__salesperson.sql
@@ -5,16 +5,17 @@ WITH salesperson_raw AS (
 
 , renamed AS (
     SELECT
-        CAST(BusinessEntityID AS INT) AS pk_salesperson
-      ,  CAST(TerritoryID AS INT) AS fk_territory
-      ,  COALESCE(CAST(SalesQuota AS FLOAT), 0) AS sales_quota
-      ,  COALESCE(CAST(Bonus AS FLOAT), 0) AS bonus
-      ,  CAST(CommissionPct AS FLOAT) AS commission_pct
-      ,  CAST(SalesYTD AS FLOAT) AS sales_ytd
-      ,  CAST(SalesLastYear AS FLOAT) AS sales_last_year
-      ,  UPPER(CAST(RowGuid AS STRING)) AS id_unique
-      ,  DATE_TRUNC('DAY', CAST(ModifiedDate AS TIMESTAMP)) AS modified_date  
+         CAST(BusinessEntityID AS INT) AS pk_salesperson -- pk do vendedor
+       , CAST(TerritoryID AS INT) AS fk_territory -- FK para território de vendas
+       , COALESCE(CAST(SalesQuota AS FLOAT), 0) AS sales_quota -- Meta de vendas do vendedor
+       , COALESCE(CAST(Bonus AS FLOAT), 0) AS bonus -- Bônus recebido pelo vendedor
+       , CAST(CommissionPct AS FLOAT) AS commission_pct -- Percentual de comissão do vendedor
+       , CAST(SalesYTD AS FLOAT) AS sales_ytd -- Total de vendas do ano atual
+       , CAST(SalesLastYear AS FLOAT) AS sales_last_year -- Total de vendas do último ano
+       , UPPER(CAST(RowGuid AS STRING)) AS id_unique -- Identificador único do registro
+       , DATE_TRUNC('DAY', CAST(ModifiedDate AS TIMESTAMP)) AS modified_date -- Data de última modificação
     FROM salesperson_raw
 )
 
 SELECT * FROM renamed
+
